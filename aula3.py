fc68f225326ef1655d4acdba793707c760bfb517
Lamaterazo <arthurresende321@gmail.com>
2022-10-07 19:21:42 -0300

    primeira alteração do projeto

---
+++ aula3.py
@@ -0,0 +1,13 @@
+print('Hello world')
+
+texto = 'Informe o primeiro valor: '
+valor1 = int(input(texto))
+valor2 = input('Informe o segundo valor: ')
+print('O primeiro valor informado foi: ', valor1,
+      '\nO segundo valor informado foi: ',valor2)
+
+print(int(valor1) + int(valor2))
+
+nome = input('Qual é o seu nome?')
+
+print ('Olá, %s .' % nome)
