f19cd3e20393c06cf54a3843c4e44b06f8e2c56e
Lamaterazo <arthurresende321@gmail.com>
2022-10-17 19:34:44 -0300

    terceira alteração

--- aula4.py
+++ aula4.py
@@ -1,33 +1,117 @@
-from tkinter import *
-janela = Tk()
-janela["bg"] = "white"
-app = Frame(janela)
-app.grid()
+import tkinter as tk
+import mysql.connector
+#pip install mysql-connector
 
-def escrever():
-    print("O meu nome é:", entryNome.get()
-    , "e minha idade é:", entryIdade.get())
+class Usuarios:
+        def __init__(self, id, nome,sobrenome,cidade,estado,data_nascimento):
+                self.id = id
+                self.nome = nome
+                self.sobrenome = sobrenome
+                self.cidade = cidade
+                self.estado = estado
+                self.data_nascimento = data_nascimento
+
+
+def conexao():
+        try:
+                conexao = mysql.connector.connect(
+                        host = "localhost",
+                        user = "root",
+                        passwd = "",
+                        db = "banco_python"
+                )
+                print("conectado")
+                return conexao
+        except mysql.connector.Error as e:
+                print(f'Erro ao conectar no Servidor MySql: {e}')
+
+def desconectar(conexao):
+        if conexao:
+                conexao.close()
+
+def selecionarUsuarios():
+        conn = conexao()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM USUARIOS")
+
+def cadastrarUsuarios():
+    janelaUsuarios = tk.Toplevel(app)
+
+    lblNome = tk.Label(janelaUsuarios,text="Informe o seu nome: "
+            ,font="Times"
+            ,bg="white",foreground="black")
+    lblNome.place(x=100,y=50)
+
+    entryNome = tk.Entry(janelaUsuarios)
+    entryNome.place(x=230,y=55)
     
-labelMsg = Label(janela,text="Qual seu nome?:"
-    ,font="Times"
-    ,bg="white",foreground="black")
-labelMsg.place(x=1,y=1)
+    lblSobrenome = tk.Label(janelaUsuarios,text="Informe o seu sobrenome: "
+            ,font="Times"
+            ,bg="white",foreground="black")
+    lblSobrenome.place(x=100,y=75)
+    entrySobrenome = tk.Entry(janelaUsuarios)
+    entrySobrenome.place(x=260, y=75)
 
-entryNome = Entry(janela)
-entryNome.place(x=109,y=1)
+    lblDataNascimento = tk.Label(janelaUsuarios,text="Informe sua data de nascimento"
+            ,font="Times"
+            ,bg="white", foreground="black")
+    lblDataNascimento.place(x=100, y=100)
+    entryDataNascimento = tk.Entry(janelaUsuarios)
+    entryDataNascimento.place(x=300, y=100)
 
-Labelmsg = Label(janela,text="Qual sua idade?:",font="Times",bg="white",foreground="black")
-Labelmsg.place(x=1,y=22)
+    lblCidade = tk.Label(janelaUsuarios,text="Informe a sua cidade"
+            ,font="Times"
+            ,bg="white", foreground="black")
+    lblCidade.place(x=100,y=125)
+    entryCidade = tk.Entry(janelaUsuarios)
+    entryCidade.place(x=230,y=125)
 
-entryIdade = Entry(janela)
-entryIdade.place(x=109,y=22)
+    lblEstado = tk.Label(janelaUsuarios, text="Informe o estado: "
+            ,font="Times"
+            ,bg="white",foreground="black")
+    lblEstado.place(x=100, y=150)
+    entryEstado = tk.Entry(janelaUsuarios)
+    entryEstado.place(x=230, y=150)
+    
+    def salvarUsuario():
+        conn = conexao()
+        print("O nome informado foi: ",entryNome.get())
+        print("O sobrenome informado foi: ", entrySobrenome.get())
+        print("A data de nascimento informada foi: ", entryDataNascimento.get())
+        print("A cidade informada foi: ", entryCidade.get())
+        print("O estado informado foi: ",entryEstado.get())
+    btnSalvar = tk.Button(janelaUsuarios,width=20
+            ,text="Salvar", command=salvarUsuario)
+    btnSalvar.place(x=100,y=175)
+    
+    #entryNome.insert("end","teste")
+    #entryNome.insert("end","tormes")
+    
+    janelaUsuarios.title("Cadastro de Usuários")
+    janelaUsuarios.geometry("800x600")
+def cadastrarProdutos():
+    janelaProduto = tk.Toplevel(app)
+    janelaProduto.title("Cadastro de Produtos")
+    janelaProduto.geometry("800x600")
+app = tk.Tk()
 
-btnFalarNome = Button(janela,width=20,text="Clicar", command=escrever)
-btnFalarNome.place(x=100,y=42)
+menuPrincipal = tk.Menu(app)
+app.config(menu=menuPrincipal)
 
-title="Sistema Tarumã"
-janela.title(title)
-janela.geometry("800x400")
-janela.resizable(False,False)
-janela.mainloop()
-janela.destroy()
+fileMenu = tk.Menu(menuPrincipal)
+fileMenu.add_command(label="Cadastrar Usuários"
+            ,command=cadastrarUsuarios)
+fileMenu.add_command(label="Cadastrar Produtos"
+            ,command=cadastrarProdutos)
+menuPrincipal.add_cascade(label="Funcao"
+                        ,menu=fileMenu)
+
+#buttonExample = tk.Button(app, 
+#              text="Create new window",
+#              command=createNewWindow)
+#buttonExample.place(x=100,y=50)
+app.title("Sistema Tarumã")
+app.geometry("800x600")
+
+app.mainloop()
+app.destroy()
